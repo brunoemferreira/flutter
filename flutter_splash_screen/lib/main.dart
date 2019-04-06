7f3441684acb6912e4902dabcaf9043ac44e6015
bruno <brunoemf@gmail.com>
2019-04-06 17:15:11 -0300

    no message

--- flutter_splash_screen/lib/main.dart
+++ flutter_splash_screen/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp( MaterialApp(
   theme:
-  ThemeData(primaryColor: Colors.red, accentColor:  Colors.yellowAccent),
+  ThemeData(primaryColor: Colors.red, accentColor:  Colors.white),
   debugShowCheckedModeBanner: false,
   home: SplashScreen(),
 ));
